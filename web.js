91f0360e5bb5eac0a97f847ae3ebc89edb9c8e21
titu3e <75441242+titu3e@users.noreply.github.com>
2022-01-27 09:36:23 +0530

    Update web.js

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, "0.0.0.0", () => {
+var server = app.listen(process.env.PORT, "zhongli-image.herokuapp.com", () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Web server started at http://%s:%s', host, port);
